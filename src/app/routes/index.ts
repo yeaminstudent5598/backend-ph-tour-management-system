6f7365861112b9dab1331019779f53c45824b653
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-08-12 10:06:25 +0600

    part-5 done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 
 export const router = Router()
@@ -13,6 +15,14 @@ const moduleRoutes = [
     {
         path: "/auth",
         route: AuthRoutes
+    },
+    {
+        path: "/division",
+        route: DivisionRoutes
+    },
+    {
+        path: "/tour",
+        route: TourRoutes
     }
 ]
 moduleRoutes.forEach((route) => {
