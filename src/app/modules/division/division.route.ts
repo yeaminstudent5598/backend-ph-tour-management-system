4a2d29a1b2dadf6823138671322178797345da07
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-08-12 10:06:25 +0600

    part-5 done

--- src/app/modules/division/division.route.ts
+++ src/app/modules/division/division.route.ts
@@ -1,14 +1,29 @@
 import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "../user/user.interface";
+import { DivisionController } from "./division.controller";
+import {
+    createDivisionSchema,
+    updateDivisionSchema,
+} from "./division.validation";
 import { validationRequest } from "../../middlewares/validateRequest";
-
-
 
 const router = Router()
 
-router.post("/create", 
-    checkAuth(Role.ADMIN, Role.SUPER_ADMIN), 
+router.post(
+    "/create",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
     validationRequest(createDivisionSchema),
     DivisionController.createDivision
-)
+);
+router.get("/", DivisionController.getAllDivisions);
+router.get("/:slug", DivisionController.getSingleDivision)
+router.patch(
+    "/:id",
+    checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
+    validationRequest(updateDivisionSchema),
+    DivisionController.updateDivision
+);
+router.delete("/:id", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), DivisionController.deleteDivision);
+
+export const DivisionRoutes = router
--- src/app/modules/tour/tour.interface.ts
+++ src/app/modules/tour/tour.interface.ts
@@ -1,4 +1,5 @@
 import { Types } from "mongoose";
+
 export interface ITourType {
     name: string;
 }
@@ -6,17 +7,19 @@ export interface ITour {
     title: string;
     slug: string;
     description?: string;
-    images? : string[];
-    location? : string;
-    costFrom ?: number;
-    startDate ?: Date
-    endDate? : Date;
-    included? : string[];
-    excluded ?: string[];
-    amenities ?: string[];
-    tourPlan? : string[];
-    maxGuest? : number;
-    minAge? : number;
-    division :  Types.ObjectId;
-    tourType: Types.ObjectId;
+    images?: string[];
+    location?: string;
+    costFrom?: number;
+    startDate?: Date
+    departureLocation?: string;
+    arrivalLocation?: string;
+    endDate?: Date;
+    included?: string[];
+    excluded?: string[]
+    amenities?: string[];
+    tourPlan?: string[];
+    maxGuest?: number;
+    minAge?: number;
+    division: Types.ObjectId
+    tourType: Types.ObjectId
 }
